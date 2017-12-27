e133d10b788470d885fd953779c8d5be69bf10d0
Unknown <grishma@jeavio.com>
2017-12-27 19:38:59 +0530

    react examples

---
+++ 27 dec 17/ReactClassComponentsUsingYarnInstaller/components/a.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Book extends React.Component {
+    render() {
+        return (
+            <div style={{color: 'red'}}>
+                <h1>This is from Book Class.....</h1>
+            </div>);
+    }
+}
